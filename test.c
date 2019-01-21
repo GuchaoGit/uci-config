fa98864baa3af3f9ba4aea451a3e199318c26a83
guc <happygc@126.com>
2019-01-21 17:30:26 +0800

    get options info

--- test.c
+++ test.c
@@ -12,7 +12,7 @@ void load_config()
 {
 
 	struct uci_package* pkg = NULL;
-	struct uci_element *e;
+	struct uci_element *e,*oe;
 	ctx = uci_alloc_context();//申请UCI上下文
 	int result;
 	if((result=uci_load(ctx,UCI_CONFIG_FILE,&pkg))!=UCI_OK){
@@ -23,16 +23,22 @@ void load_config()
 	printf("open config file success\n");
 
 
-	uci_foreach_element(&pkg->sections,e){
+	uci_foreach_element(&pkg->sections,e){//遍历所有的section
 		struct uci_section *s = uci_to_section(e);//将element转换为section类型
 
 		printf("uci type = %d ,uci name =  %s\n",(s->e).type,(s->e).name);
 
-		if(NULL != (value = uci_lookup_option_string(ctx,s,"apn"))){
-			apn = strdup(value);
-			printf("apn = %s\n",apn);	
-		}
 
+		//遍历section下options
+		uci_foreach_element (&s->options,oe){
+			struct uci_option *o = uci_to_option(oe);
+			printf("uci type = %d ,uci name =  %s\n",(o->e).type,(o->e).name);
+		
+			if(NULL != (value = uci_lookup_option_string(ctx,s,(o->e).name))){
+				printf("%s = %s\n\n",(o->e).name,value);
+			}
+
+		}
 		printf("\n\n");
 
 	}
@@ -65,9 +71,9 @@ int main(void)
 {
 	load_config();
 
-	modifyApn("wan","myapn");
+//	modifyApn("wan","myapn");
 
-	load_config();
+//	load_config();
 
 
 	return 0;
